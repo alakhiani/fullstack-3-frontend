cf88de8ec49b5e207b07f69524b8dbc7d76bcd9e
Avinash Lakhiani <avinash.lakhiani@gmail.com>
2023-05-09 19:11:15 -0700

    Added links to header to navigate to each page. Also cleaned the default home page code when the project was created.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,18 @@
 import { Container } from "@mui/material";
 import Header from "./Header";
 import Footer from "./Footer";
+import Link from "next/link";
 
 export default function Layout({ children }) {
     return (
         <>
-            <Header title="John Doe"></Header>
+            <Header title="John Doe">
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+                <Link href="/projects">Projects</Link>
+                <Link href="/contact">Contact</Link>
+                <Link href="/admin">Admin</Link>
+            </Header>
             <Container fixed>
                 <main>{children}</main>
             </Container>
