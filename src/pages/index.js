3addbd33d1a1c031942621894914e7b4e7f4d856
Avinash Lakhiani <alakhiani@yahoo.com>
2023-05-09 19:34:26 -0700

    Added button to navigate to projects.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,24 @@
+import styles from "../styles/Home.module.css";
+import { Button } from "@mui/material";
+import  { useRouter } from "next/router";
+
 export default function Home( { name, summary } ) {
+
+  const router = useRouter();
+
   return (
-    <section className="{style.Home}">
-      <h1 className="{style.Name}">{name}</h1>
-      <div className="{style.Summary}">{summary}</div>
+    <section className={styles.Home}>
+      <h1 className={styles.Name}>{name}</h1>
+      <div className={styles.Summary}>{summary}</div>
+      <div>
+        <Button
+          variant="contained"
+          size="large"
+          onClick={() => router.push("/projects")}
+        >
+          Projects
+        </Button>
+      </div>
     </section>
   )
 }
